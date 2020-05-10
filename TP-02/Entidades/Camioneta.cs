af1e13213ded2574e7268fc7e5231c8a809d4a57
alessandra.fernandes@naturalth.it <alessandra.fernandes@naturalth.it>
2020-05-10 02:39:14 -0300

    subiendo Tp-02

---
+++ TP-02/Entidades/Camioneta.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Camioneta : Vehiculo
+    {
+        public Camioneta(EMarca marca, string chasis, ConsoleColor color)
+            : base(marca, chasis, color)
+        {
+        }
+        /// <summary>
+        /// Las camionetas son grandes
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Grande;
+            }
+        }
+
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("CAMIONETA");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine($"TAMAÑO : {this.Tamanio}");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
